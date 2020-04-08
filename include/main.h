599bb37c1c88e09e9b5d44ffa9144bae6c0b8082
silasejohn <paradox687a@gmail.com>
2020-04-08 15:31:26 -0700

    Update main.h

--- include/main.h
+++ include/main.h
@@ -21,14 +21,16 @@ using namespace okapi;
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void autonomous(void);
 void initialize(void);
 void disabled(void);
 void competition_initialize(void);
 void opcontrol(void);
+
 #ifdef __cplusplus
 }
-#endif
+#endif //cleaning up formatting
 
 #ifdef __cplusplus
 
