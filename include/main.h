ff7e0826dbf19414960f8e46386b805f08d3ab13
silasejohn <paradox687a@gmail.com>
2020-04-08 16:45:41 -0700

    Main.h explanations added

--- include/main.h
+++ include/main.h
@@ -1,13 +1,28 @@
-#ifndef _PROS_MAIN_H_
-#define _PROS_MAIN_H_
+#ifndef _PROS_MAIN_H_ //has this file/line been defined earlier or in an included file? (If not defined)
+#define _PROS_MAIN_H_ //include gaurd?
 
+/**
+ * If defined, some commonly used enums will have preprocessor macros which give
+ * a shorter, more convenient naming pattern. If this isn't desired, simply
+ * comment the following line out.
+ *
+ * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
+ * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
+ * not convienent for most student programmers.
+ */
 #define PROS_USE_SIMPLE_NAMES
 
+/**
+ * If defined, C++ literals will be available for use. All literals are in the
+ * pros::literals namespace.
+ *
+ * For instance, you can do `4_mtr = 50` to set motor 4's target velocity to 50
+*/
 #define PROS_USE_LITERALS
 
 // #define DEBUG
 
-// Nice includes below
+// Nice includes below (dependecies)
 #include "api.h"
 #include "pros/apix.h"
 #include "pros/rtos.h"
@@ -18,8 +33,8 @@
 
 using namespace okapi;
 
-#ifdef __cplusplus
-extern "C" {
+#ifdef __cplusplus //im guessing this means, if this is a c++ file, and we want to include the following c code
+extern "C" {       //do the following
 #endif
 
 void autonomous(void);
@@ -32,19 +47,20 @@ void opcontrol(void);
 }
 #endif //cleaning up formatting
 
-#ifdef __cplusplus
+#ifdef __cplusplus //if (not completely sure about this part yet so come back to it)
 
-#include <ostream>
-#include <iostream>
-#include <chrono>
-#include <ctime>
-#include <cstdlib>
+#include <ostream> //output streams
+#include <iostream> //input and output streams
+#include <chrono> //time
+#include <ctime> //time
+#include <cstdlib> //randomness, conversions, with other things as well
 
-#include <vector>
+#include <vector> //ArrayList basically
 
-#include <stdio.h>
-#include <stdarg.h>
-
+#include <stdio.h> //input output langauge but used in C? (not c++)
+                   //<cstdio> in c++, but uses printf, removing all docs and stuff
+#include <stdarg.h> //also for c, but basically unlimited arguments to a function (unknown typr and number)
+// this is basically variadic functions (write your params, then an ellipses for unknowns)
 #endif
 
 #endif  // _PROS_MAIN_H_
